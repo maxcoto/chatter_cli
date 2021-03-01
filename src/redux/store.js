6f6b567a2a4a1d81216e3082bdae95f45d73170c
maxiperezc <maxiperezc@gmail.com>
2021-03-01 12:00:46 -0300

    redix dev tools optional

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,7 @@ const store = createStore(
       routerMiddleware(history), // for dispatching history actions
       // ... other middlewares ...
     ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   ),
 )
 
